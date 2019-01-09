ee71f262fe1952ee15e2d7676af23a5fa377048d
Augustin Godiscal <dev@auggod.io>
2019-01-09 12:16:27 +0100

    Use bigger list

--- fetch-words.js
+++ fetch-words.js
@@ -1,12 +1,13 @@
 var fs = require('fs')
 var got = require('got')
 
-var url = 'https://raw.github.com/atebits/Words/master/Words/en.txt'
+var url = 'https://raw.github.com/dwyl/english-words/master/words_alpha.txt'
 
 got(url).then(res => {
   var words = res.body
     .trim()
     .split('\n')
+    .map((s) => s.trim())
     .filter(w => w.length === 4)
 
   fs.writeFileSync('words.txt', words.join('\n'))
